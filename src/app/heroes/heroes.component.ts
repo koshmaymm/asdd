502a3f4024dd244460f1800a61fc26ec41730a1a
Mikhail <koshmaymm@ukr.net>
2020-06-12 18:12:49 +0300

    change

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss'],
+})
+export class HeroesComponent implements OnInit {
+  hero: Hero = {
+    id: 12,
+    name: 'Windstorm',
+  };
+
+  constructor() {}
+
+  ngOnInit() {}
+}
